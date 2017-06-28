7fc3ee9869845a0e7d15e6f159552be3b7d89ddb
BooleanCat <thgodkin@gmail.com>
2017-06-28 23:07:31 +0100

    add shellcheck test

---
+++ scripts/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RELEASE_DIR="$( dirname "$DIR" )"
+
+shellcheck "${DIR}/test.sh"
+find "${RELEASE_DIR}/packages" -name packaging -print0 | xargs -0 shellcheck
