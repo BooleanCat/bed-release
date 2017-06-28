2b37b17521b9332fab7f5da2c5568b130d671566
BooleanCat <thgodkin@gmail.com>
2017-06-28 23:44:16 +0100

    add link to python dependencies

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,3 +6,4 @@ RELEASE_DIR="$( dirname "$DIR" )"
 
 shellcheck "${DIR}/test.sh"
 find "${RELEASE_DIR}/packages" -name packaging -print0 | xargs -0 shellcheck
+shellcheck "${RELEASE_DIR}/jobs/broker/templates/python"
