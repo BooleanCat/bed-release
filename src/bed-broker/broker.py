2d625a34f1825e9aafa8a21d25e7638cfb251a69
BooleanCat <thgodkin@gmail.com>
2017-07-03 21:43:54 +0100

    randomise broker password when deploying

--- src/bed-broker/broker.py
+++ src/bed-broker/broker.py
@@ -1,10 +1,18 @@
+import os
+import json
 from flask import Flask
 from flask_basicauth import BasicAuth
 
+def load_config():
+    config_path = os.environ['BROKER_CONFIG']
+    with open(config_path) as config_file:
+      return json.load(config_file)
+
 def setup_basic_auth(app):
+    config = load_config()
     app.config['BASIC_AUTH_FORCE'] = True
-    app.config['BASIC_AUTH_USERNAME'] = 'foo'
-    app.config['BASIC_AUTH_PASSWORD'] = 'bar'
+    app.config['BASIC_AUTH_USERNAME'] = config['username']
+    app.config['BASIC_AUTH_PASSWORD'] = config['password']
     return BasicAuth(app)
 
 app = Flask(__name__)
