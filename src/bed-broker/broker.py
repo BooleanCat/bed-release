87664fa08d46f1e5ff1b317f1bc9f14eb6f32ab0
Tom Godkin <tgodkin@pivotal.io>
2017-07-03 13:08:20 +0100

    enable basic auth for broker

--- src/bed-broker/broker.py
+++ src/bed-broker/broker.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_basicauth import BasicAuth
+
+def setup_basic_auth(app):
+    app.config['BASIC_AUTH_FORCE'] = True
+    app.config['BASIC_AUTH_USERNAME'] = 'foo'
+    app.config['BASIC_AUTH_PASSWORD'] = 'bar'
+    return BasicAuth(app)
 
 app = Flask(__name__)
-
+basic_auth = setup_basic_auth(app)
 
 @app.route('/ping')
 def ping():
     return 'pong'
-
 
 if __name__ == "__main__":
     app.run()
